42b02ecc00385ca173d82b8cfe365466b334fa73
Korkeat Wannpat <korkeat.w@gmail.com>
2021-11-13 00:50:39 +0700

    Also work on Claim and Mine

--- farmersworld-auto-claim.js
+++ farmersworld-auto-claim.js
@@ -1,6 +1,7 @@
-const DELAY_EACH_ITEM_SEC = 10 * 1000
-const DELAY_AFTER_READ_ALL_ITEMS_SEC = 30 * 1000
-const DELAY_AFTER_CLICKED_ITEM_SEC = 1 * 1000
+const DELAY_EACH_ITEM = 10 * 1000
+const DELAY_AFTER_READ_ALL_ITEMS = 30 * 1000
+const DELAY_AFTER_CLICKED_ITEM = 1 * 1000
+const POPUP_APPEAR_TIMER = 1 * 1000
  
  
 async function delay(ms=1000) {
@@ -12,21 +13,32 @@ function currentDatetime() {
   return d.toISOString()
 }
 
+function closeModal(text) {
+  const modalConfirmBtn=document.getElementsByClassName('plain-button short undefined')[0]
+  
+  if (modalConfirmBtn && modalConfirmBtn.innerText.toLowerCase() === 'ok') {
+    console.log(text);
+    modalConfirmBtn.click();
+  }
+}
+
 async function claim(itemName) {
   const claimBtn=document.getElementsByClassName('button-section set-height')[0]
 
-  if (claimBtn && claimBtn.innerText === 'Mine') {
+  if (claimBtn && claimBtn.innerText.toLowerCase() === 'mine') {
     claimBtn.click();
-    console.log(`Claimed ${itemName} at ${currentDatetime()}`)
+    console.log(`Mined ${itemName} at ${currentDatetime()}`)
     
-    await delay(DELAY_AFTER_CLICKED_ITEM_SEC)
+    await delay(POPUP_APPEAR_TIMER)
     
-    const modalConfirmBtn=document.getElementsByClassName('plain-button short undefined')[0]
-  
-    if (modalConfirmBtn && modalConfirmBtn.innerText === 'OK') {
-      console.log(`You got new ${itemName}!`);
-      modalConfirmBtn.click();
-    }
+    closeModal(`You already mined ${itemName}`)
+  } else if (claimBtn && claimBtn.innerText.toLowerCase() === 'claimed') {
+    claimBtn.click();
+    console.log(`!! Claimed ${itemName} at ${currentDatetime()}`)
+    
+    await delay(POPUP_APPEAR_TIMER)
+    
+    closeModal(`You got new ${itemName}!`)
   }
 }
 
@@ -39,14 +51,14 @@ while(true) {
     
     for (item of items) {
       item.click()
-      await delay(DELAY_AFTER_CLICKED_ITEM_SEC)
+      await delay(DELAY_AFTER_CLICKED_ITEM)
       
       const itemName = document.querySelector("div.info-title-name").innerText
       
       await claim(itemName)
-      await delay(DELAY_EACH_ITEM_SEC)
+      await delay(DELAY_EACH_ITEM)
     }
   }
 
-  await delay(DELAY_AFTER_READ_ALL_ITEMS_SEC)
+  await delay(DELAY_AFTER_READ_ALL_ITEMS)
 }

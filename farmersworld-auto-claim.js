bcd29f5757f365d6391acb9f0ef42084c248ad60
Korkeat Wannpat <korkeat.w@gmail.com>
2021-12-10 12:10:26 +0700

    Add auto recharge energy feature

--- farmersworld-auto-claim.js
+++ farmersworld-auto-claim.js
@@ -1,8 +1,9 @@
-const DELAY_EACH_ITEM = 10 * 1000
+const DELAY_EACH_ITEM = 20 * 1000
 const DELAY_AFTER_READ_ALL_ITEMS = 30 * 1000
 const DELAY_AFTER_CLICKED_ITEM = 1 * 1000
 const POPUP_APPEAR_TIMER = 1 * 1000
- 
+const ENERGY_THRESHOLD_PERCENT = 60
+
  
 async function delay(ms=1000) {
   return new Promise(resolve => setTimeout(resolve, ms))
@@ -42,6 +43,39 @@ async function claim(itemName) {
   }
 }
 
+function percentage(a, b) {
+  return parseFloat(((a/b)*100).toFixed(2))
+}
+
+function rechargeEnergy() {
+  const resources = document.querySelectorAll(".resource__group")
+
+  if (resources.length) {
+    const energyNumbers = resources[3].innerText.split('\n/')
+    const remainEnergyPercent = percentage(energyNumbers[0], energyNumbers[1])
+    
+    // if energy lower than theshold, fill it
+    if (remainEnergyPercent < ENERGY_THRESHOLD_PERCENT) {
+      console.log(`Remain energy is lower than threshold: ${remainEnergyPercent}%`)
+      console.log('Filling the energy ...')
+
+      // click + to add energy
+      document.querySelector('.resource-energy--plus').click()
+
+      // increase energy number to be filled, find and click exchange button
+      do {
+        var energyValue = document.querySelector(".modal-input").value
+        document.querySelector("img[alt='Plus Icon']").click()
+      } while (energyValue != document.querySelector(".modal-input").value)
+
+      Array.from(document.querySelectorAll("div.plain-button"))
+        .find(elm => elm.textContent == 'Exchange').click()
+    } else {
+      console.log('Energy is equal or greater than threshold. Do nothing!')
+    }
+  }
+} 
+
 
 while(true) {
   const itemsElm=document.querySelector("section.vertical-carousel-container")
@@ -50,6 +84,8 @@ while(true) {
     const items=itemsElm.children
     
     for (item of items) {
+      rechargeEnergy()
+
       item.click()
       await delay(DELAY_AFTER_CLICKED_ITEM)
       

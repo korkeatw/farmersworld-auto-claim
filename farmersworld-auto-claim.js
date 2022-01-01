883613f2dee7cb104eda4560999742a7b214a408
Korkeat W <407286+korkeatw@users.noreply.github.com>
2022-01-01 12:01:42 +0700

    Recharge energy when browser tab not active (#3)
    * Work around for the modal is not closing
    
    * Experiment: change recharge energy logic, some user report the code inactive when browser tab not active
    
    * Check if energy value is not zero
    
    Co-authored-by: Korkeat Wannapat <korkeat.wan@ascendcorp.com>

--- farmersworld-auto-claim.js
+++ farmersworld-auto-claim.js
@@ -27,6 +27,10 @@ function closeModal(text) {
   if (modalConfirmBtn && modalConfirmBtn.innerText.toUpperCase() === 'OK') {
     console.log(text);
     modalConfirmBtn.click();
+  } else {
+    // work around in case of the program cannot find OK button
+    // just click any elemen outside modal to close
+    document.getElementById('root').click()
   }
 }
 
@@ -92,14 +96,27 @@ async function rechargeEnergy() {
       await delay(DEFAULT_DELAY)
 
 
-      // increase energy number to be filled, find and click exchange button
-      do {
-        var energyValue = document.querySelector(".modal-input").value
-        document.querySelector("img[alt='Plus Icon']").click()
-      } while (energyValue != document.querySelector(".modal-input").value)
+      // input energy number to be filled
+      const energyToBeFilled = energyNumbers[1] - energyNumbers[0]
 
+      while (true) {
+        const energyValue = document.querySelector(".modal-input").value
+        
+        if (energyValue && parseInt(energyValue) > 0 && parseInt(energyValue) < energyToBeFilled) {
+            document.querySelector("img[alt='Plus Icon']").click()
+        } else {
+          console.log(`Energy value is invalid or be maximal value (${energyValue}). Stop increasing the value.`)
+          break
+        }
+      }
+
+      console.log(`energy to be filled is = ${document.querySelector(".modal-input").value}`)
+
+      // click "Exchange" button to submit
       Array.from(document.querySelectorAll("div.plain-button"))
         .find(elm => elm.textContent == 'Exchange').click()
+
+      await delay(5000)
     } else {
       console.log('Energy is equal or greater than threshold. Do nothing!')
     }
